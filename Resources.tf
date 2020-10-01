f42de5835ba009cd94f13b668a04dbccad83abe8
Piotr Palka <piotr.palka123@gmail.com>
2020-10-01 16:17:59 +0200

    Using proper cidr block

--- Resources.tf
+++ Resources.tf
@@ -85,7 +85,7 @@ resource "aws_route_table" "route_table_internet" {
   vpc_id = aws_vpc.vpc.id
 
   route {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.internet_gateway.id
   }
 
