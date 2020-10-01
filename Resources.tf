5225a5e55e29e62dedd4cd073298f939c9bec725
Piotr Palka <piotr.palka123@gmail.com>
2020-10-01 15:05:30 +0200

    List syntax fix

--- Resources.tf
+++ Resources.tf
@@ -82,7 +82,7 @@ resource "aws_internet_gateway" "internet_gateway" {
 resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.elastic_ip_nat_gateway.id
   subnet_id = aws_subnet.public_subnet.id
-  depends_on = aws_internet_gateway.internet_gateway
+  depends_on = [ aws_internet_gateway.internet_gateway ]
 
   tags = {
     Name = "Nat Gateway"
