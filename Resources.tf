09fb6f842c41c83f1f912e7f6cee538c0d1694e8
Piotr Palka <piotr.palka123@gmail.com>
2020-10-01 13:02:34 +0200

    Creating private and public subnets

--- Resources.tf
+++ Resources.tf
@@ -19,9 +19,56 @@
 #   }
 # }
 
+
+#
+# VPC
+#
+
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = "Smart Appartment Data recruitment task"
   }
 }
+
+
+#
+# Subnets
+#
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "Public subnet 1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Public subnet 2"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.0.128.0/24"
+
+  tags = {
+    Name = "Private subnet 1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.0.129.0/24"
+
+  tags = {
+    Name = "Private subnet 2"
+  }
+}
+
