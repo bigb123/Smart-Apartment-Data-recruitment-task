c726ba43abb8ae479d676d0819b1a6b9db5facbb
Piotr Palka <piotr.palka123@gmail.com>
2020-10-02 23:50:41 +0200

    security groups update

--- Resources.tf
+++ Resources.tf
@@ -258,6 +258,7 @@ resource "aws_launch_template" "nginx_template" {
   # ubuntu: ami-0817d428a6fb68645
   instance_initiated_shutdown_behavior = "terminate"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ aws_security_group.allow_http.id ]
   
   # metadata_options {
   #   http_endpoint               = "enabled"
