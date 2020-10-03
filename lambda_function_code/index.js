d3c9154600dea48a9b3e77518167124f0718632c
Piotr Palka <piotr.palka123@gmail.com>
2020-10-03 17:18:56 +0200

    checking internal and external sites

--- lambda_function_code/index.js
+++ lambda_function_code/index.js
@@ -5,15 +5,30 @@
 //   }
 
 const https = require('https')
-let url = "https://docs.aws.amazon.com/lambda/latest/dg/welcome.html"
+const http = require('http')
+let url_external = "https://docs.aws.amazon.com/lambda/latest/dg/welcome.html"
+let url_internal = "10.0.129.161"
 
 exports.handler = async function(event) {
-  const promise = new Promise(function(resolve, reject) {
-    https.get(url, (res) => {
+  const promise_internal = new Promise(function(resolve, reject) {
+    http.get(url_internal, (res) => {
         resolve(res.statusCode)
       }).on('error', (e) => {
         reject(Error(e))
       })
     })
-  return promise
+  
+  console.log("response from internal site:", promise_internal)
+
+  const promise_external = new Promise(function(resolve, reject) {
+    https.get(url_external, (res) => {
+        resolve(res.statusCode)
+      }).on('error', (e) => {
+        reject(Error(e))
+      })
+    })
+
+  console.log("response from internal site:", promise_external)
+
+  return promise_external
 }
