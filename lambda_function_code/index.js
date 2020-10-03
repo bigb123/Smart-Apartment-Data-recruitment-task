a320ebd0559bd3abbf1bd18a676ecd2e46e58194
Piotr Palka <piotr.palka123@gmail.com>
2020-10-03 17:03:10 +0200

    added request to external webpage

--- lambda_function_code/index.js
+++ lambda_function_code/index.js
@@ -1,5 +1,19 @@
-exports.handler =  async function(event, context) {
-    console.log("This lambda function seems to be working");
-    console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-    return context.logStreamName
-  }
+// exports.handler =  async function(event, context) {
+//     console.log("This lambda function seems to be working");
+//     console.log("EVENT: \n" + JSON.stringify(event, null, 2))
+//     return context.logStreamName
+//   }
+
+const https = require('https')
+let url = "https://docs.aws.amazon.com/lambda/latest/dg/welcome.html"
+
+exports.handler = async function(event) {
+  const promise = new Promise(function(resolve, reject) {
+    https.get(url, (res) => {
+        resolve(res.statusCode)
+      }).on('error', (e) => {
+        reject(Error(e))
+      })
+    })
+  return promise
+}
