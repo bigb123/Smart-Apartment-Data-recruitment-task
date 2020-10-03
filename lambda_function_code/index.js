ac2d303ec9c62a48da569e84ef7d3dc26f885996
Piotr Palka <piotr.palka123@gmail.com>
2020-10-03 17:36:03 +0200

    log update and explanatory comment added

--- lambda_function_code/index.js
+++ lambda_function_code/index.js
@@ -1,15 +1,26 @@
-// exports.handler =  async function(event, context) {
-//     console.log("This lambda function seems to be working");
-//     console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-//     return context.logStreamName
-//   }
+/*
+ * This function is checking access to external and internal sites. However,
+ * internal EC2 instance that is running Ńginx in private subnet has non-static
+ * ip address. To avoid hardcoding it, I will have to 
+ * 1 - read the ip address from the EC2 instance during runtime, or
+ * 2 - set up the internal load balancer, or
+ * 3 - use the elastic ip
+ * 
+ * As none of these is a part of the task specification I decided to 
+ * follow the easiest way by hardcoding ip address of the instance 
+ * into the Lambda function that proves the VPC is configured properly
+ * and the Lambda function can access web pages placed externally and 
+ * internally.
+ */
 
 const https = require('https')
 const http = require('http')
 let url_external = "https://docs.aws.amazon.com/lambda/latest/dg/welcome.html"
-let url_internal = "http://10.0.129.161"
+let url_internal = "http://10.0.129.161" // Change the ip addr according to
+// the ip addr of your instance in the private subnet
 
 exports.handler = async function(event) {
+  
   const promise_internal = new Promise(function(resolve, reject) {
     http.get(url_internal, (res) => {
         resolve(res.statusCode)
@@ -28,7 +39,7 @@ exports.handler = async function(event) {
       })
     })
 
-  console.log("response from internal site:", await promise_external)
+  console.log("response from external site:", await promise_external)
 
   return promise_external
 }
